1655a11e9624aa19df94c6a721f80fb1849babdc
latifah <kimtifah2@gmail.com>
2024-01-16 00:12:21 +0200

    changes

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,12 +1,12 @@
 @extends('layouts.app')
 @section('content')
-<div id="home" class="parallax first-section wow fadeIn" data-stellar-background-ratio="0.4" style="background-image:url('images/slider-bg.png');" {{ $app = App\Models\general_settings::latest()->first() }}>
+<div id="home" class="parallax first-section wow fadeIn" data-stellar-background-ratio="0.4" style="background-image:url('photos/bulb.jpg');" {{ $app = App\Models\general_settings::latest()->first() }}>
    <div class="container">
       <div class="row">
          <div class="col-md-12 col-sm-12">
             <div class="text-contant">
                <h2>
-                  <a href="" class="typewrite" data-period="2000" data-type='[ "Welcome to Waste2wealth", "We care about your work", "We provide electricity to the society!" ]'>
+                  <a href="" class="typewrite" data-period="2000" data-type='[ "Welcome to Waste2wealth", "We care about your work", "We provide electricity to the society!" ]'  style="color:white;">
                   <span class="wrap"></span>
                   </a>
                </h2>
@@ -22,7 +22,7 @@
    <div class="container">
       <div class="col-lg-4 col-md-4 col-sm-12 col-xs-12">
          <div class="row">
-            <div class="service-time one" style="background:#2895f1;">
+            <div class="service-time one" style="background:#2A1F3F;">
                <span class="info-icon"><i class="fa fa-car" aria-hidden="true"></i></span>
                <h3>Our products</h3>
                <p>We provide electricity to households, companies, electric vehicles like cars and motorcycles </p>
@@ -31,7 +31,7 @@
       </div>
       <div class="col-lg-4 col-md-4 col-sm-12 col-xs-12">
          <div class="row">
-            <div class="service-time middle" style="background:#0071d1;">
+            <div class="service-time middle" style="background:#2A1F3F;">
                <span class="info-icon"><i class="fa fa-clock-o" aria-hidden="true"></i></span>
                <h3>We are open 24/7</h3>
                <p>You are free to buy electricity anytine and anywhere</p>
@@ -40,7 +40,7 @@
       </div>
       <div class="col-lg-4 col-md-4 col-sm-12 col-xs-12">
          <div class="row">
-            <div class="service-time three" style="background:#0060b1;">
+            <div class="service-time three" style="background:#2A1F3F;">
                <span class="info-icon"><i class="fa fa-money" aria-hidden="true"></i></span>
                <h3>Cost</h3>
                <p>The electricity we sell is affordable and this was also our goal in producing it so that everyone can afford to use it</p>
@@ -81,14 +81,14 @@
       <hr class="hr1">
       <div class="row">
           <div class="heading">
-         <span class="icon-logo"><img src="{{ ($app) ? 'storage/'.$app->icon_logo_path:'images/icon-logo.png' }}" alt="#"></span>
+         <!--<span class="icon-logo"><img src="{{ ($app) ? 'storage/'.$app->icon_logo_path:'images/icon-logo.png' }}" alt="#"></span>-->
          <h2>OUR TARGET CUSTOMERS</h2>
       </div>
          <div class="col-md-3 col-sm-6 col-xs-12">
             <div class="service-widget">
                <div class="post-media wow fadeIn">
-                  <a href="images/clinic_01.jpg" data-rel="prettyPhoto[gal]" class="hoverbutton global-radius"><i class="flaticon-unlink"></i></a>
-                  <img src="images/clinic_01.jpg" alt="" class="img-responsive">
+                  <a href="photos/house.jpg" data-rel="prettyPhoto[gal]" class="hoverbutton global-radius"><i class="flaticon-unlink"></i></a>
+                  <img src="photos/house.jpg" alt="" class="img-responsive">
                </div>
                <h3>Individuals(households)</h3>
             </div>
@@ -97,8 +97,8 @@
          <div class="col-md-3 col-sm-6 col-xs-12">
             <div class="service-widget">
                <div class="post-media wow fadeIn">
-                  <a href="images/clinic_02.jpg" data-rel="prettyPhoto[gal]" class="hoverbutton global-radius"><i class="flaticon-unlink"></i></a>
-                  <img src="images/clinic_02.jpg" alt="" class="img-responsive">
+                  <a href="photos/go  kabisa.jpeg" data-rel="prettyPhoto[gal]" class="hoverbutton global-radius"><i class="flaticon-unlink"></i></a>
+                  <img src="photos/go  kabisa.jpeg" alt="" class="img-responsive">
                </div>
                <h3>Go Kabisa</h3>
             </div>
@@ -107,8 +107,8 @@
          <div class="col-md-3 col-sm-6 col-xs-12">
             <div class="service-widget">
                <div class="post-media wow fadeIn">
-                  <a href="images/clinic_03.jpg" data-rel="prettyPhoto[gal]" class="hoverbutton global-radius"><i class="flaticon-unlink"></i></a>
-                  <img src="images/clinic_03.jpg" alt="" class="img-responsive">
+                  <a href="photos/ampersand.jpg" data-rel="prettyPhoto[gal]" class="hoverbutton global-radius"><i class="flaticon-unlink"></i></a>
+                  <img src="photos/ampersand.jpg" alt="" class="img-responsive">
                </div>
                <h3>Ampersand</h3>
             </div>
@@ -117,8 +117,8 @@
          <div class="col-md-3 col-sm-6 col-xs-12">
             <div class="service-widget">
                <div class="post-media wow fadeIn">
-                  <a href="images/clinic_01.jpg" data-rel="prettyPhoto[gal]" class="hoverbutton global-radius"><i class="flaticon-unlink"></i></a>
-                  <img src="images/clinic_01.jpg" alt="" class="img-responsive">
+                  <a href="photos/university.jpeg" data-rel="prettyPhoto[gal]" class="hoverbutton global-radius"><i class="flaticon-unlink"></i></a>
+                  <img src="photos/university.jpeg" alt="" class="img-responsive">
                </div>
                <h3>Organizations</h3>
             </div>
@@ -129,7 +129,7 @@
    </div>
    <!-- end container -->
 </div>
-<!--<div id="service" class="services wow fadeIn">
+<div id="service" class="services wow fadeIn">
     <div class="container">
        <div class="row">
           <div class="col-lg-8 col-md-8 col-sm-6 col-xs-12">
@@ -137,31 +137,31 @@
                 <div class="col-lg-4 col-md-6 col-sm-6 col-xs-12">
                    <div class="serv">
                       <span class="icon-service"><img src="images/service-icon1.png" alt="#" /></span>
-                      <h4>PREMIUM FACILITIES</h4>
-                      <p>Lorem Ipsum is simply dummy text of the printing.</p>
+                      <h4>INSTALLATION</h4>
+                      <p>Reach out for you want to install this electricity.</p>
                    </div>
                 </div>
                 <div class="col-lg-4 col-md-6 col-sm-6 col-xs-12">
                    <div class="serv">
                       <span class="icon-service"><img src="images/service-icon2.png" alt="#" /></span>
-                      <h4>LARGE LABORATORY</h4>
-                      <p>Lorem Ipsum is simply dummy text of the printing.</p>
+                      <h4>FIXING/ REPAIRING</h4>
+                      <p>In need of repairing feell free to call us and we will be happy to help</p>
                    </div>
                 </div>
                 <div class="col-lg-4 col-md-6 col-sm-6 col-xs-12">
                    <div class="serv">
                       <span class="icon-service"><img src="images/service-icon3.png" alt="#" /></span>
-                      <h4>DETAILED SPECIALIST</h4>
-                      <p>Lorem Ipsum is simply dummy text of the printing.</p>
+                      <h4>JOB opportunities</h4>
+                      <p>If you are interested in what we do and you want a job  you can also contact us</p>
                    </div>
                 </div>
-                <div class="col-lg-4 col-md-6 col-sm-6 col-xs-12">
+                <!--<div class="col-lg-4 col-md-6 col-sm-6 col-xs-12">
                    <div class="serv">
                       <span class="icon-service"><img src="images/service-icon4.png" alt="#" /></span>
                       <h4>CHILDREN CARE CENTER</h4>
                       <p>Lorem Ipsum is simply dummy text of the printing.</p>
                    </div>
-                </div>
+                </div>-->
                 <div class="col-lg-4 col-md-6 col-sm-6 col-xs-12">
                    <div class="serv">
                       <span class="icon-service"><img src="images/service-icon5.png" alt="#" /></span>
@@ -183,7 +183,6 @@
     </div>
  </div>
  <!-- end section -->
--->
 <!-- doctor -->
 
 <!-- <div id="doctors" class="parallax section db" data-stellar-background-ratio="0.4" style="background:#fff;" data-scroll-id="doctors" tabindex="-1">
@@ -324,7 +323,7 @@
                   <!-- end row -->
                </div>
                <!-- end pane -->
-              <!-- <div class="tab-pane fade" id="tab2">
+              <div class="tab-pane fade" id="tab2">
                   <div class="row text-center">
                      <div class="col-md-6">
                         <div class="pricing-table">
